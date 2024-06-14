5ed8135eb1a0fb218ad2633d0baea7f2ff659f36
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-06-14 10:44:32 +0600

    sorting and search designed

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -11,7 +11,23 @@ const Orders = () => {
       </div>
 
       <div className="overflow-x-auto mt-10">
-  <table className="table table-zebra">
+      <div className="flex items-center justify-between">
+          <div className="">
+            <label className='text-sm font-semibold'>Sort By</label>
+            <select className="select select-bordered w-full max-w-xs border-primary select-sm">
+                <option disabled selected>Sort By</option>
+                <option>Date</option>
+                <option>Name</option>
+                <option>Price High to Low</option>
+                <option>Price Low to High</option>
+              </select>
+          </div>
+          <label className="input input-bordered input-sm border-primary flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+          </label>
+        </div>
+  <table className="table table-zebra mt-5">
     {/* head */}
     <thead>
       <tr>
