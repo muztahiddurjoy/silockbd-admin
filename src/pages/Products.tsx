2cbf24d862fac88a6617cc42f0c6fb19aaac839b
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-07-30 14:41:17 +0600

    product filter and search done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,7 +10,9 @@ import { db } from '../firebase'
 const Products = () => {
   const [products, setproducts] = useState<DocumentData[]>([])
   const [categories, setcategories] = useState<DocumentData[]>([])
-
+  const [temparr, settemparr] = useState<Product[]>([])
+  const [searchQuery, setsearchQuery] = useState('')
+  const [selectedCat, setselectedCat] = useState('')
   const getCategories = async  ()=>{
     const result = await getDocs(collection(db,'categories'))
     setcategories(result.docs)
@@ -25,6 +27,49 @@ const Products = () => {
     getCategories()
     getProducts()
   }, [])
+
+  const resetTempArr = ()=>{
+    settemparr(products.map((v)=>{
+      const product:Product = v?.data()
+      return product
+  }))
+  }
+
+  useEffect(() => {
+    resetTempArr()
+  }, [products])
+
+  useEffect(() => {
+    resetTempArr()
+    const simplified = searchQuery.toLowerCase().trim()
+    if(!simplified){
+      resetTempArr()
+    }
+    else{
+      settemparr(p=>
+        p.filter((v)=>
+          String(v.category).toLowerCase().trim().includes(simplified)||
+          String(v.codeNumber).toLowerCase().trim().includes(simplified)||
+          String(v.name).toLowerCase().trim().includes(simplified)||
+          String(v.description).toLowerCase().trim().includes(simplified)
+        ))
+    }
+  }, [searchQuery])
+
+  useEffect(() => {
+    resetTempArr()
+    if(selectedCat=="all"){
+      resetTempArr()
+    }
+    else{
+      settemparr((p)=>p.filter((v)=>{
+        return String(v.category).toLowerCase().trim().includes(selectedCat.toLowerCase().trim())
+      }))
+    }
+  }, [selectedCat])
+  
+  
+  
   
   return (
     <Dashboard>
@@ -36,17 +81,18 @@ const Products = () => {
         <div className="flex items-center justify-between">
           <div className="">
             <label className='text-sm font-semibold'>Category Filter</label>
-            <select className="select select-bordered w-full max-w-xs border-primary select-sm">
+            <select className="select select-bordered w-full max-w-xs border-primary select-sm" onChange={e=> setselectedCat(e.target.value)}>
                 <option disabled selected>Category</option>
                 {categories.map((v,i)=>{
                       if(v?.exists()){
                         return <option key={i} value={v?.data().category}>{v?.data().category}</option>
                       }            
                 })}
+                <option value="all">All</option>
               </select>
           </div>
           <label className="input input-bordered input-sm border-primary flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
+            <input type="text" value={searchQuery} onChange={e=> setsearchQuery(e.target.value)} className="grow" placeholder="Search" />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
           </label>
         </div>
@@ -68,12 +114,7 @@ const Products = () => {
       </tr>
     </thead>
     <tbody>
-      {products.map((v,i)=>{
-        if(v?.exists()){
-          const product:Product = v?.data()
-          return <TableAdapter {...product} key={i}/>
-        }
-      })}
+      {temparr.map((v,i)=><TableAdapter {...v} key={i}/>)}
     </tbody>
     
     <tfoot>
