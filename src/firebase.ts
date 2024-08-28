2e0bd297ff4b722d902009c24fe1a76b47f90de8
Muztahid Durjoy <43501336+muztahiddurjoy@users.noreply.github.com>
2024-08-28 16:13:47 +0600

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-import {getStorage} from 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBMV9N03F3zPkpJUKR5KQJsTPcC2AAxnpc",
-  authDomain: "silockbangladesh.firebaseapp.com",
-  projectId: "silockbangladesh",
-  storageBucket: "silockbangladesh.appspot.com",
-  messagingSenderId: "889562469739",
-  appId: "1:889562469739:web:e8d44ac01de66bcd8c682e"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const storage = getStorage(app)
-export const db = getFirestore(app)
